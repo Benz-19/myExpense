066974ba4c22c7a4e6885a6ba765be4513f77aaf
ugwu_kingsley <kingsleyikenna2019@gmail.com>
2025-05-01 21:24:09 +0300

    update

--- index.php
+++ index.php
@@ -4,11 +4,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <!-- Core Css -->
+    <link rel="stylesheet" href="./public/css/index.css">
+    <title>myExpense</title>
 </head>
 
 <body>
-
+    <section class="main-container glass">
+        <div class="logo-cont">
+            <img src="./public/images/logo (2).png" alt="">
+        </div>
+        <div class="options">
+            <div class="login">
+                <button>Login</button>
+            </div>
+            <div class="sign-up">
+                <button>Sign up</button>
+            </div>
+        </div>
+    </section>
 </body>
 
 </html>
