ba2065e1f757bf6847f1851fa00266a611b1c795
ugwu_kingsley <kingsleyikenna2019@gmail.com>
2025-05-02 15:14:06 +0300

    update

--- app/Models/db.php
+++ app/Models/db.php
@@ -1,11 +1,14 @@
 <?php
 //dir app/Models/db.php
+declare(strict_types=1);
+
 namespace App\Models;
 
+use PDO;
+use PDOException;
 use Dotenv\Dotenv;
 
 require_once __DIR__ . "/../../vendor/autoload.php";
-
 
 
 class DB
@@ -14,6 +17,9 @@ class DB
     private $password;
     private $host;
     private $dbname;
+    protected $conn;
+
+
     public function __construct()
     {
         $dotenv = Dotenv::createImmutable(__DIR__ . '/../../');
@@ -26,6 +32,37 @@ class DB
 
     public function connection()
     {
-        return $this->dbname;
+        try {
+            $dsn = "mysql:host={$this->host}, dbname={$this->dbname}";
+            $this->conn = new PDO($dsn, $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            echo "DB Connection error: " . $e->getMessage();
+        }
+
+        return $this->conn;
+    }
+
+    //SQL Query execution
+    static function execute(string $query,  $params = [])
+    {
+        $sql = self::connection()->prepare($query);
+        return $sql->execute($params);
+    }
+
+    //SQL Query fetch a single data
+    static function fetchSingleData(string $query,  $params = [])
+    {
+        $sql = self::connection()->prepare($query);
+        $sql->execute($params);
+        return $sql->fetch(PDO::FETCH_ASSOC);
+    }
+
+    //SQL Query fetch all data
+    static function fetchAllData(string $query,  $params = [])
+    {
+        $sql = self::connection()->prepare($query);
+        $sql->execute($params);
+        return $sql->fetchAll(PDO::FETCH_ASSOC);
     }
 }
