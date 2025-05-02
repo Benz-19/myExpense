e557efe773a953c58abbfcc5ce10c5012d469341
ugwu_kingsley <kingsleyikenna2019@gmail.com>
2025-05-02 13:18:46 +0300

    update

--- index.php
+++ index.php
@@ -2,12 +2,18 @@
 
 $request = $_SERVER['REQUEST_URI'];
 
+$request = str_replace('/myExpense', '', $request); //replaces the default url with '/'
+
 echo $request;
+
 $viewDir = '/resources/Views/';
 switch ($request) {
     case '':
     case '/':
         require __DIR__ . $viewDir . 'landing.php';
+        break;
+    case '/dashboard/':
+        require __DIR__ . $viewDir . 'dashboard.php';
         break;
     default:
         require __DIR__ . $viewDir . '404.php';
