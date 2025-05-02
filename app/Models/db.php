d64d189fedf08fdb5e9d72e82d05f7d47bd5f481
ugwu_kingsley <kingsleyikenna2019@gmail.com>
2025-05-02 15:24:07 +0300

    update

--- app/Models/db.php
+++ app/Models/db.php
@@ -33,7 +33,7 @@ class DB
     public function connection()
     {
         try {
-            $dsn = "mysql:host={$this->host}, dbname={$this->dbname}";
+            $dsn = "mysql:host={$this->host};dbname={$this->dbname};charset=utf8mb4";
             $this->conn = new PDO($dsn, $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
