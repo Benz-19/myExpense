a91705dce08cf68138f329d8c611e1f457c93a15
Benz-19 <kingsleyikenna2019@gmail.com>
2025-05-04 12:09:42 +0300

    Update signup.php

--- resources/Views/signup.php
+++ resources/Views/signup.php
@@ -1,3 +1,15 @@
+<?php
+session_start();
+if (isset($_SESSION['error_message'])) {
+    echo $_SESSION['error_message'];
+    unset($_SESSION['error_message']);
+}
+
+if (isset($_SESSION['success_message'])) {
+    echo $_SESSION['success_message'];
+    unset($_SESSION['success_message']);
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
