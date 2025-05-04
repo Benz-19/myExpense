14d8c680f7b0759b7e016e86f761cf30a16b9d87
Benz-19 <kingsleyikenna2019@gmail.com>
2025-05-04 11:36:51 +0300

    Update landing.php

--- resources/Views/landing.php
+++ resources/Views/landing.php
@@ -4,6 +4,92 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Core Css -->
+    <!-- <link rel="stylesheet" href="./public/css/index.css"> -->
+    <style>
+        body,
+        html {
+            margin: 0;
+            padding: 0;
+            height: 100%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background-color: #f9f9f9;
+            /* Optional: light background */
+        }
+
+        .main-container {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            box-shadow: 12px 12px 10px rgba(176, 175, 175, 0.708);
+            padding: 40px;
+            max-width: 90%;
+            width: 400px;
+            background-color: white;
+            border-radius: 10px;
+        }
+
+        /* Logo */
+        .logo-cont img {
+            width: 40px;
+            height: auto;
+        }
+
+        .logo-cont {
+            padding: 50px 0 30px;
+        }
+
+        /* Button Options */
+        .options {
+            display: flex;
+            justify-content: space-between;
+            width: 100%;
+            padding-top: 10px;
+        }
+
+        /* Shared button styles */
+        .login button,
+        .sign-up button {
+            width: 100%;
+            height: 40px;
+            color: white;
+            cursor: pointer;
+            transition: all 0.3s ease-in-out;
+            border-radius: 5px;
+            font-size: 1rem;
+            border: none;
+        }
+
+        .login {
+            flex: 1;
+            margin-right: 10px;
+        }
+
+        .login button {
+            background-color: rgb(20, 203, 20);
+        }
+
+        .login button:hover {
+            background-color: grey;
+            transform: translateY(-10%);
+        }
+
+        .sign-up {
+            flex: 1;
+            margin-left: 10px;
+        }
+
+        .sign-up button {
+            background-color: rgb(28, 28, 171);
+        }
+
+        .sign-up button:hover {
+            background-color: grey;
+            transform: translateY(-10%);
+        }
+    </style>
     <title>myExpense</title>
 </head>
 
