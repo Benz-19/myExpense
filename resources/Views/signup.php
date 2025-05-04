7587ce6c9bbdba47b4dca5ac106b6df23619719a
Benz-19 <kingsleyikenna2019@gmail.com>
2025-05-04 12:42:33 +0300

    Update signup.php

--- resources/Views/signup.php
+++ resources/Views/signup.php
@@ -1,13 +1,49 @@
 <?php
 
-if (isset($_SESSION['error_message'])) {
-    echo $_SESSION['error_message'];
-    unset($_SESSION['error_message']);
+//Error Message Handler   
+if (isset($_SESSION['success_message'])) {
+    echo '<script>const msg = true;</script>';
+    echo '<div class ="success">' . $_SESSION['success_message'] . '</div>';
+    unset($_SESSION['success_message']);
+    ?>
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+        var success_msgs = document.querySelectorAll('.success');
+    
+        if (success_msgs.length > 0) {
+            success_msgs.forEach(function(msgElement) {
+                msgElement.style.color = 'green';
+            });
+    
+            setTimeout(() => {
+                window.history.back(); //redirect to the previous page
+            }, 9000);
+        }
+    });
+    </script>
+    <?php
 }
 
-if (isset($_SESSION['success_message'])) {
-    echo $_SESSION['success_message'];
-    unset($_SESSION['success_message']);
+//Success Message Handler 
+if (isset($_SESSION['error_message'])) {
+    echo '<div class ="error">' . $_SESSION['success_message'] . '</div>';
+    unset($_SESSION['error_message']);
+    ?>
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            var error_msg = document.querySelectorAll('.error');
+            if (error_msg.length > 0) {
+                error_msg.forEach(function(msgElm){
+                    msgElm.style.color = 'red';
+                });
+                
+                setTimeout(() => {
+                    window.history.back(); //redirect to the previous page
+                }, 9000);
+            }
+        });
+    </script>
+    <?php
 }
 ?>
 <!DOCTYPE html>
